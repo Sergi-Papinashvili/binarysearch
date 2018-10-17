9d0e9cbf738fd8f3c4bf4573f0e70138321ac4e5
maxo1z <maxinio55@gmail.com>
2018-10-17 18:04:12 +0400

    binarysearch

---
+++ script.js
@@ -0,0 +1,68 @@
+var i = [1, 2, 3, 4, 6, 5];
+var swap =0;
+var k = 0;
+var middleindex=0;
+var middle =0;
+for (var x = 0; x<i.length; x++) {
+
+ for(var y = 0; y<i.length-1; y++) {
+
+
+
+    if (i[y]>i[y+1]) {
+         
+        swap = i[y];
+        i[y] = i[y+1];
+        i[y+1]=swap;
+        k=1;
+
+    
+    }
+        
+ 
+    }
+
+
+var c=prompt("sheiyvanet ricxvi");
+
+
+
+
+    if (k==0)  {
+        
+                  alert("massivi aris dalagebuli");
+       
+       
+               }
+
+        
+            }
+
+
+            if (i.length%2==0)  {
+
+               middleindex =i.length/2;
+               middle=i[middleindex];
+               
+
+               
+               if(c>middle)   {
+                
+                for (var z =0; )
+                
+
+
+               }
+              
+
+
+
+
+            }
+
+
+
+            
+
+
+
